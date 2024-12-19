47bb54bbd47488b7c47ba2be5e9e62b34182c033
mtsgmz07 <matissegompro@gmail.com>
2024-12-19 10:41:57 +0100

    fix bug component

---
+++ src/ui/buttons/Button.jsx
@@ -0,0 +1,11 @@
+export const Button = ({ props }) => {
+  switch (props.variant) {
+    case 'primary':
+      return (
+        <>
+          <button></button>
+        </>
+      )
+      break
+  }
+}
