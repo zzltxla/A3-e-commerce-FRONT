4ed70168f476cdf52099a8c88881bbcca2e070f8
zzltxla <taazazel@gmail.com>
2024-12-19 13:19:12 +0100

    add:icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css'
 import Homepage from './pages/Homepage';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 function App() {
 
@@ -9,6 +11,10 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
+
+        <Route path='/register' element={<Register/>}/>
+        
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </Router>
     </>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,22 @@
 import { Input } from "../components/Inputs/InputText"
 import { Button } from "../ui/buttons/Button"
 
-export default function Login () {
+export default function Register () {
     return (
         <>
             <main>
                 <div className="form-login-register">
                     <form action="login" method="post">
+
+                        <Input variant="primary"    name="first name"></Input>
+                        <Input variant="primary"    name="last name"></Input>
+                        <Input variant="primary" name="phone"></Input>
                         <Input variant="primary" name="email"></Input>
                         <Input variant="primary" name="password"></Input>
-                        <a href="/register">Don&apos;t have an account yet ? Create one </a>
-                        <Button variant="primary" >Log In</Button>
+
+                        <a href="/login">Already have an account ? Log in </a>
+
+                        <Button variant="primary" >Register</Button>
                     </form>
                 </div>
             </main>
