cf5bf50a793814e256cee0d8cdf3db0b819be4c8
“Ziyad <ziyadclombe.pro@gmail.com>
2024-12-19 11:44:32 +0100

    Homepage en cours

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,20 @@
+import {CartIcon} from '../ui/svg/CartIcon';
+import {AccountIcon} from '../ui/svg/AccountIcon';
+
 export const Header = ({title}) => {
     document.title = title;
     return (
         <>
         <header>
             <ul>
-                <li>SHOP.CO</li>
-                <li>Shop</li>
-                <li>On sale</li>
-                <li>New Arrivals</li>
-                <li>Brands</li>
-                <li></li>
-                <li></li>
+                <li><a href="#">SHOP.CO</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="">On sale</a></li>
+                <li><a href="#">New Arrivals</a></li>
+                <li><a href="#">Brands</a></li>
+                <li><input type="text" placeholder="Rechercher un produit"/></li>
+                <li><a href="#"> <CartIcon/> </a></li>
+                <li><a href="#"><AccountIcon/></a></li>
             </ul>
         </header>
         </>
